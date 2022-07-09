0468e13b841adcc809f85610a88bb4a47e913965
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-07-09 19:33:36 +0900

    refactoring

--- main.cpp
+++ main.cpp
@@ -145,9 +145,11 @@ void ExpandWildcard(const std::string& line, automaton_sizes_t& automaton_sizes,
   }
   int size = MinimizedDFASize(line.c_str());
   automaton_sizes[size]++;
-  #pragma omp critical
   if (size <= output_automaton_size_max) {
-    found.emplace_back(line);
+    #pragma omp critical
+    {
+      found.emplace_back(line);
+    }
   }
 }
 
