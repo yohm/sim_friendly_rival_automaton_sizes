8dbacc92cd6ca6bd33394332d3f04b302100cfad
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-07-11 00:34:02 +0900

    updated the job script for fugaku

--- job/fugaku_job.sh
+++ job/fugaku_job.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#PJM --rsc-list "node=12"
-#PJM --rsc-list "rscgrp=small"
-#PJM --rsc-list "elapse=00:10:00"
+#PJM --rsc-list "node=800"
+#PJM --rsc-list "rscgrp=large"
+#PJM --rsc-list "elapse=04:00:00"
 #PJM --mpi "max-proc-per-node=4"
 #PJM -s
 
 export OMP_NUM_THREADS=12
 
-mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../main.out fr_list_1M
-#mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../main.out ../friendly_rival_10k
+mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../../main.out dis.passed 9
