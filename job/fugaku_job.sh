7bca2fd151619166b6e9b2158b0192461c8a4878
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-07-15 11:26:52 +0900

    updated parameters

--- job/fugaku_job.sh
+++ job/fugaku_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PJM --rsc-list "node=800"
+#PJM --rsc-list "node=400"
 #PJM --rsc-list "rscgrp=large"
-#PJM --rsc-list "elapse=04:00:00"
+#PJM --rsc-list "elapse=06:00:00"
 #PJM --mpi "max-proc-per-node=4"
 #PJM -s
 
 export OMP_NUM_THREADS=12
 
-mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../../main.out dis.passed 9
+mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../../main.out dis.passed 7 0
